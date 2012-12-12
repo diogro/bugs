6f4538fb54c3023f98dddba084ec6702b3aa2b0a
diogro <diogro@gmail.com>
2012-12-12 16:28:29 -0200

    Read data

--- cinco.sp.r
+++ cinco.sp.r
@@ -1,14 +1,16 @@
 require("rjags")
-schools  <- read.table("schools.dat", header = T)
-J  <- nrow(schools)
-y  <- schools$estimate
-sigma.y  <- schools$sd
-data  <- list("J" = J, "y" = y, "sigma.y" = sigma.y)
+cinco.sp  <- read.table("dados5sp.csv", header = T, sep = ',')
+traits = list()
+for (sp in 1:5){
+    traits[[sp]]  <- cinco.sp[(60*(sp-1)):(60*sp), 1:4]
+}
+names(traits)  <- c("A", "B", "C", "D", "E")
+
 inits <- function()
     list (theta = rnorm(J, 0, 100), mu.theta = rnorm(1, 0, 100),
           sigma.theta = runif(1, 0, 100))
 parameters  <- c("theta", "mu.theta", "sigma.theta")
-school.sim  <- jags.model("model.bug", data,
+school.sim  <- jags.model("model.bug", traits,
                     n.chains = 3, n.adapt = 10000)
 update(school.sim, 10000)
 samples  <- jags.samples(school.sim, parameters, 10000)
