cc465cde82187c315b30184faf5489c229486726
diogro <diogro@gmail.com>
2012-12-12 16:45:13 -0200

    Started Model

--- cinco.sp.r
+++ cinco.sp.r
@@ -5,12 +5,16 @@ for (sp in 1:5){
     traits[[sp]]  <- cinco.sp[(60*(sp-1)):(60*sp), 1:4]
 }
 names(traits)  <- c("A", "B", "C", "D", "E")
+n.traits = 4
+n.sp = 5
+n.ind = 60
+data  <- list("traits" = traits, "n.traits" = n.traits, "n.sp" = n.sp, "n.ind" = n.ind)
 
 inits <- function()
     list (theta = rnorm(J, 0, 100), mu.theta = rnorm(1, 0, 100),
           sigma.theta = runif(1, 0, 100))
 parameters  <- c("theta", "mu.theta", "sigma.theta")
-school.sim  <- jags.model("model.bug", traits,
+school.sim  <- jags.model("model.bug", data,
                     n.chains = 3, n.adapt = 10000)
 update(school.sim, 10000)
 samples  <- jags.samples(school.sim, parameters, 10000)
